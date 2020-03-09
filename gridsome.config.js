af3080c1c16a66db67689a3808ac45e3583e201d
suXin <suxinjke@gmail.com>
2020-03-09 21:16:44 +0700

    remove copy pasted commentary regarding RSS

--- gridsome.config.js
+++ gridsome.config.js
@@ -52,10 +52,8 @@ module.exports = {
         {
             use: 'gridsome-plugin-feed',
             options: {
-                // Required: array of `GraphQL` type names you wish to include
                 contentTypes: [ 'Notes' ],
-                // Optional: any properties you wish to set for `Feed()` constructor
-                // See https://www.npmjs.com/package/feed#example for available properties
+
                 feedOptions: {
                     title: 'suXin space',
                     link: `${siteUrl}/notes/`,
@@ -68,22 +66,14 @@ module.exports = {
                         link: `${siteUrl}/`
                     }
                 },
-                // === All options after this point show their default values ===
-                // Optional; opt into which feeds you wish to generate, and set their output path
+
                 rss: {
                     enabled: true,
                     output: '/feed.xml'
                 },
-                // Optional: the maximum number of items to include in your feed
                 maxItems: 25,
-                // Optional: an array of properties passed to `Feed.addItem()` that will be parsed for
-                // URLs in HTML (ensures that URLs are full `http` URLs rather than site-relative).
-                // To disable this functionality, set to `null`.
                 htmlFields: [ 'description', 'content' ],
 
-                // Optional: a method that accepts a node and returns an object for `Feed.addItem()`
-                // See https://www.npmjs.com/package/feed#example for available properties
-                // NOTE: `date` field MUST be a Javascript `Date` object
                 nodeToFeedItem: ( node ) => ( {
                     title: node.title,
                     date: new Date( node.date ),
