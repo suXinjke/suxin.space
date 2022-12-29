fa25f4c10f8e0944eb4f804eb59b881500e63703
suXin <suxinjke@gmail.com>
2022-12-29 20:40:20 +0700

    allow to specify styles for images, like float

--- frontend/components/image.tsx
+++ frontend/components/image.tsx
@@ -1,4 +1,6 @@
 import { h } from 'nano-jsx'
+import * as url from 'url'
+import * as querystring from 'querystring'
 
 export interface ImageProps {
   href: string
@@ -6,12 +8,17 @@ export interface ImageProps {
 }
 
 export default function Image({ href, title }: ImageProps) {
+  const { pathname, query } = url.parse(href)
+  const parsedQuery = querystring.parse(query)
+
   return (
-    <div class="image">
-      <a href={href} target="_blank" rel="noopener">
-        <img src={href} title={title} />
-      </a>
-      {title && <h4 class="image__title">{title}</h4>}
+    <div class="image" style={Object.keys(parsedQuery).length > 0 ? parsedQuery : undefined}>
+      <div class="image__container">
+        <a href={href} target="_blank" rel="noopener">
+          <img src={href} title={title} />
+        </a>
+        {title && <h4 class="image__title">{title}</h4>}
+      </div>
     </div>
   )
 }
